31c77f13550ea6d87b537dd21b4e1d032910d27d
MillionPears <trieuleb4@gmail.com>
2024-11-02 10:20:55 +0700

    add common-library and parent pom.xml

---
+++ common-library/src/main/java/com/laundry/common_library/CommonLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.laundry.common_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonLibraryApplication.class, args);
+	}
+
+}
